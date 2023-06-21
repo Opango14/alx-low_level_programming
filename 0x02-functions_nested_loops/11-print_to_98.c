ace1590175ed3030cf7e56aceb3fdff825e9e58b
Timothy Opango <opangotimothy14@gmail.com>
2023-06-21 00:00:41 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void print_to_98(int n)
 		for (i = n; i<= 98; i++)
 		{
 			if (i != 98)
-				_putchar('i');
+				printf("%d, ", i);
 			else if (i == 98)
-				_putchar('\n');
+				printf("%d\n", i);
 		}
 	}
 	else if (n >=98)
@@ -23,9 +23,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				_putchar('j');
+				printf("%d, ", j);
 			else if (j == 98)
-				_putchar('\n');
+				printf("%d\n", j);
 		}
 	}
 }
